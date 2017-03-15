2ef84252479c456d2a42f775775a288a369d32c6
SamCatherman <sam.catherman@gmail.com>
2017-03-15 14:35:33 -0400

    adds sign up, sign in, sign out forms & logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #sets session for current user
-  before_action :authenticate_user  
+  before_action :authenticate_user! 
 end
