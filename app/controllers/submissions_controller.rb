31784dd71982224f0a393be4d522892c707ba6ea
SamCatherman <sam.catherman@gmail.com>
2017-03-15 21:48:09 -0400

    adds migration, restricts user access at controller level

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -10,33 +10,35 @@ class SubmissionsController < ApplicationController
   end
 #initializes new submission, only if user is logged in
   def new
-    redirect_to root_path unless @current_user
     @submission = Submission.new
   end
 #saves new submission, only if user is logged in
   def create
-    redirect_to root_path unless @current_user
-    @submission = Submission.create(submission_params)
+    @submission = current_user.submissions.create!(submission_params)
     flash[:notice] = "submission created"
     redirect_to @submission
   end
 #saves edited submission information to database, only if user is logged in
   def update
-    redirect_to root_path unless @current_user
     @submission = Submission.find(params[:id])
     @submission.update(submission_params)
     redirect_to @submission
   end
 #finds submission by ID, displays edit page if user matches
   def edit
-    redirect_to root_path unless @current_user
-    @submission = Submission.find(params[:id])
+    # if @submission.user == current_user
+      @submission = Submission.find(params[:id])
+    # end
+
   end
 #deletes submission only if user is logged in
   def destroy
-    redirect_to root_path unless @current_user
     @submission = Submission.find(params[:id])
-    @submission.destroy
+    if @submission.user == current_user
+      @submission.destroy
+    else
+      flash[:alert] = "Only the author of the submission can delete"
+    end
     redirect_to submissions_path
   end
 
